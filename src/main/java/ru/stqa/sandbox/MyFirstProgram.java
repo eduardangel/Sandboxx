3bf1e3f470392d8ac326ba5e306e661ad9de473a
Eduard Angelskiy <ed_angel74@mail.ru>
2024-05-03 22:59:17 +0700

    Изменил MyFirstProgram с учетом изменений в классах

--- src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -1,3 +1,5 @@
+package ru.stqa.sandbox;
+
 public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
@@ -8,14 +10,14 @@ public class MyFirstProgram {
     System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
 
     Square s = new Square(6);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     double x = 4;
     double y = 6;
     System.out.println("Площадь квадрата со сторонами " + x + " и " + y + " = " + area(x,y));
 
     Rectangle rectangle = new Rectangle(2,5);
-    System.out.println("Площадь квадрата со сторонами " + rectangle.x + " и " + rectangle.y + " = " + area(rectangle));
+    System.out.println("Площадь квадрата со сторонами " + rectangle.x + " и " + rectangle.y + " = " + rectangle.area());
   }
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
@@ -23,13 +25,7 @@ public class MyFirstProgram {
   public static double area (double l){
     return l * l;
   }
-  public static double area(Square l){
-    return l.l * l.l;
-  }
   public static double area (double width, double height){
     return width * height;
-  }
-  public static double area (Rectangle r){
-    return r.x * r.y;
   }
 }
